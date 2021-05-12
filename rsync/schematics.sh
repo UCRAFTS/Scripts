55523e7cf170075e65a63936131ea28c991c369b
Alexander Repin <alexrepinru@yandex.ru>
2021-05-12 23:40:05 +0300

    Update schematics.sh

--- rsync/schematics.sh
+++ rsync/schematics.sh
@@ -1,1 +1,11 @@
-sd
+#!/bin/bash
+
+SSH_PORT=22
+SSH_USER="user"
+SSH_HOST="localhost"
+PATH_FROM="/data/folder/1"
+PATH_TO="/data/folder/2"
+ARGS="-avPI"
+
+rm -r $PATH_TO/*
+rsync $ARGS -e "ssh -p $SSH_PORT" $SSH_USER@$SSH_HOST:$PATH_FROM $PATH_TO
